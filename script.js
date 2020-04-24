a6bdcaef424dd72205f2f083491cafe2c4293d13
Angie0603 <marosikatalin5@gmail.com>
2020-04-24 16:35:45 +0200

    end of the matreial review

--- script.js
+++ script.js
@@ -26,6 +26,12 @@ let addButtonHandler = () => {
 }
 };
 
+function addButtonHandler2() {
+    if ($('li').length < 5) {
+    $('ul').append('<li>My name is '+ name +'</li>');
+}
+};
+
 $('#add-button').click(addButtonHandler);
 
 $('#remove-button').click(() => {
@@ -35,15 +41,39 @@ $('#remove-button').click(() => {
 
 let age = 12;
 
-console.log('Starting the if')
-console.log('This is a sentence'.toLowerCase())
+console.log('Starting the if');
+console.log('This is a sentence'.toLowerCase());
 
 if(age < 18) {
-console.log('Not old enough to drink')
+console.log('Not old enough to drink');
 } else {
-console.log('Old enough to drink')
+console.log('Old enough to drink');
 }
-console.log('This is after if')
+console.log('This is after if');
+
+let colors = ['orange', 'lime', 'magenta', 'yellow', 'blue'];
+
+$('#box-container').append('<div class="box"></div>');
+$('.box:last-child').css('background', colors[0]);
+$('#box-container').append('<div class="box"></div>');
+$('.box:last-child').css('background', colors[1]);
+$('#box-container').append('<div class="box"></div>');
+$('.box:last-child').css('background', colors[2]);
+$('#box-container').append('<div class="box"></div>');
+$('.box:last-child').css('background', colors[3]);
+$('#box-container').append('<div class="box"></div>');
+$('.box:last-child').css('background', colors[4]);
+
+colors.push('purple')
+
+colors.forEach((color) => {
+    $('#box-container').append('<div class="box"></div>');
+    $('.box:last-child').css('background', color);
+});
+
+for (let i = 0; i < 100; i = i + 1) {
+    console.log(i);
+}
 
 //This is a JavaScript comment
 /*
